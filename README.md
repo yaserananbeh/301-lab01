e4f6405a27a3775ccf9b38670190aa4c0222b470
Yaser Al-Ananbeh <yaser.ananbeh74@gmail.com>
2021-04-27 03:55:16 +0300

    finish the class-02 lap

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # 301-lab01
+
+ ## Feature #1: Display images
+ 
+ **Estimate Time** 1 hour
+ **Start Time** 2AM
+ **End Time** 3AM
+ **Actual time** 1 hour
+
+
+ ## Feature #2: Allow users to vote for their favorite beast
+ 
+ **Estimate Time** 30 min
+ **Start Time** 3:30AM
+ **End Time** 3:50Am
+ **Actual time**  20min
+
+ 
+ ## Feature 3: Bootstrap
+ 
+ **Estimate Time** 30 min
+ **Start Time** 3:50AM
+ **End Time** 4:10Am
+ **Actual time** 20px
--- react/my-app/src/component/Main.js
+++ react/my-app/src/component/Main.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import HornedBeasts from './HornedBeasts';
-import one from '../images/one.jpg';
-import two from '../images/two.jpg';
-class Main extends React.Component {
-    render() {
-        // console.log(this.props);
-        return (
-            <div>
-                <HornedBeasts imageTitle={'Unicorn Head'} imageDescription={'Someone wearing a creepy unicorn head mask'} src={one} />
-                <HornedBeasts imageTitle={'Believe'} imageDescription={'I believe in unicorns, do you?' }src={two}/>
-            </div>
-        )
-    };
-}
+import Data from '../data.json';
 
+class Main extends React.Component{
+  render(){
+        let dataContent=[];
+        Data.forEach(index=>{
+            dataContent.push(<HornedBeasts name={index.title} url={index.image_url} description={index.description} />);
+        })
+      return dataContent;
+  }
+}
 export default Main;
