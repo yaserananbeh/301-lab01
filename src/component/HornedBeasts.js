5e3cf234300059c6e5b9d9e571800ae19508085b
Yaser Al-Ananbeh <yaser.ananbeh74@gmail.com>
2021-04-28 02:51:00 +0300

    finish the lab-03 requirements

--- src/component/HornedBeasts.js
+++ src/component/HornedBeasts.js
@@ -1,22 +1,39 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Card from 'react-bootstrap/Card';
+import Modal from './modal';
 class HornedBeasts extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      numOfClicks: 0
+      numOfClicks: 0,
+      show: false,
     };
   }
-    clicksHandler = () => {
-      this.setState({
-        numOfClicks: this.state.numOfClicks + 1
-      });
-    }
-    render() {
-      return (
-        <Card style={{ width: '18rem' }}>
-          <Card.Img variant="top" src={this.props.url} onClick={this.clicksHandler}/>
+  clicksHandler = () => {
+    this.setState({
+      numOfClicks: this.state.numOfClicks + 1,
+      show: this.state.show === false ? true : false
+    });
+  }
+  showModal = () => {
+    this.setState({
+      show: this.state.show === false ? true : false
+    });
+  }
+  render() {
+    return (
+      <>
+        <Modal
+          show={this.showModal}
+          display={this.state.show}
+          title={this.props.name}
+          image={this.props.url}
+          description={this.props.description}
+          numOfClicks={this.state.numOfClicks}
+        />
+        <Card style={{ width: '18rem', display: 'inline-block' }}>
+          <Card.Img variant="top" src={this.props.url} onClick={this.clicksHandler} />
           <Card.Body>
             <Card.Title>{this.props.name}</Card.Title>
             <Card.Text>
@@ -30,7 +47,8 @@ class HornedBeasts extends React.Component {
             </Card.Text>
           </Card.Body>
         </Card>
-      );
-    }
+      </>
+    );
+  }
 }
 export default HornedBeasts;
